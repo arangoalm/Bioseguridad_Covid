8d9964fb2da1771bcf21d3be75bc7c6477c1dd38
arangoalm <arango_almeiro@yahoo.es>
2021-09-30 12:29:23 -0500

    Commit capa de dominio

--- Bioseguridad.App.Dominio/Entidades/RegistroIngreso.cs
+++ Bioseguridad.App.Dominio/Entidades/RegistroIngreso.cs
@@ -6,10 +6,10 @@ namespace Bioseguridad.App.Dominio
     public class RegistroIngreso
     {
         public int id {get;set;}
-        public date fechaIngreso {get;set;}
-        public time horaIngreso {get;set;}
-        public Persona persona {get;set;}
-        public SintomasCovid sintomasCovid {get;set;}
+        public string fechaIngreso {get;set;}
+        public string horaIngreso {get;set;}
+        //public Persona persona {get;set;}
+        //public SintomasCovid sintomasCovid {get;set;}
         
     }
 }
